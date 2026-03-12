20318b5a20f3816101e9a4b777822cf63b8478b8
Pho Hoang <phonhhgcd230181@fpt.edu.vn>
2026-03-12 12:36:15 +0700

    Project Setup and Model Design

---
+++ BookStore/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string ShippingAddress { get; set; }
+        public List<Book> BookList { get; set; } = new List<Book>();
+        public string Status { get; set; } = "Pending";
+    }
+}
