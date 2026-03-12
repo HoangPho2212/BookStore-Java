250863e12b5979ee6310cbd6e5d7f50cd6fef69e
Pho Hoang <phonhhgcd230181@fpt.edu.vn>
2026-03-12 13:36:17 +0700

    refactor-applyNode

--- BookStore/MyOrderQueue.cs
+++ BookStore/MyOrderQueue.cs
@@ -3,48 +3,43 @@ using System;
 
 namespace Bookstore
 {
+    // L02_Node concept: Creating a wrapper for our data
+    public class Node
+    {
+        public Order Data;
+        public Node Next;
+        public Node(Order order) { Data = order; Next = null; }
+    }
+
     public class MyOrderQueue
     {
-        private Order[] _orders;
-        private int _front;
-        private int _rear;
-        private int _maxSize;
+        private Node _front;
+        private Node _rear;
         private int _count;
 
-        public MyOrderQueue(int size)
-        {
-            _maxSize = size;
-            _orders = new Order[_maxSize];
-            _front = 0;
-            _rear = -1;
-            _count = 0;
-        }
-
-        
         public void Enqueue(Order item)
         {
-            if (_count == _maxSize)
+            Node newNode = new Node(item);
+            if (_rear == null)
             {
-                Console.WriteLine("Queue is full!");
-                return;
+                _front = _rear = newNode;
             }
-            _rear = (_rear + 1) % _maxSize;
-            _orders[_rear] = item;
+            else
+            {
+                _rear.Next = newNode;
+                _rear = newNode;
+            }
             _count++;
         }
 
-
         public Order Dequeue()
         {
-            if (_count == 0) return null;
-
-            Order item = _orders[_front];
-            _orders[_front] = null; // Clear reference
-            _front = (_front + 1) % _maxSize; // Circular logic
+            if (_front == null) return null;
+            Order data = _front.Data;
+            _front = _front.Next;
+            if (_front == null) _rear = null;
             _count--;
-            return item;
+            return data;
         }
-
-        public int Count => _count;
     }
 }
