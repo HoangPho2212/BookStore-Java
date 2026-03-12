f5faa3e0aab70c706f007eac2da84a1068fab291
Pho Hoang <phonhhgcd230181@fpt.edu.vn>
2026-03-12 13:46:47 +0700

    feat: integrate manual queue and quicksort logic in Program.cs

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,16 +1,54 @@
-﻿using System;
+﻿using BookStore;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BookStore
+namespace Bookstore
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
+            MyOrderQueue orderQueue = new MyOrderQueue();
+
+
+            Order order1 = new Order
+            {
+                OrderID = 101,
+                CustomerName = "Hoang Pho",
+                ShippingAddress = "123 Greenwich St",
+                BookList = new List<Book>
+                {
+                    new Book { Title = "Data Structures", Author = "Tutor A" },
+                    new Book { Title = "Algorithms", Author = "Tutor B" },
+                    new Book { Title = "C# Programming", Author = "Tutor C" }
+                }
+            };
+
+
+            Console.WriteLine("--- System: Receiving New Order ---");
+            orderQueue.Enqueue(order1);
+            Console.WriteLine($"Added Order {order1.OrderID} to the system.");
+
+
+            Console.WriteLine("\n--- System: Sorting Books for Order 101 ---");
+            AlgorithmHelper.QuickSortBooks(order1.BookList, 0, order1.BookList.Count - 1);
+
+            foreach (var book in order1.BookList)
+            {
+                Console.WriteLine($"Sorted: {book.Title} by {book.Author}");
+            }
+
+
+            Console.WriteLine("\n--- System: Processing Next Order in Queue ---");
+            Order processedOrder = orderQueue.Dequeue();
+            if (processedOrder != null)
+            {
+                Console.WriteLine($"OrderID {processedOrder.OrderID} for {processedOrder.CustomerName} is now complete.");
+            }
+
+            Console.WriteLine("\nTask finished. Press any key to close.");
+            Console.ReadKey();
         }
     }
 }
